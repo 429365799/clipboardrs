a8a49591288f6ccfef30b96ee047330bb7c4aff9
chenfenqiang <429365799@qq.com>
2023-09-01 00:05:33 +0800

    feat: complete read_clipboard_data function

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,13 @@
+use clipboard_win::raw::set_without_clear;
+use clipboard_win::{empty, get_clipboard_string, Setter, SysResult};
+use clipboard_win::{
+    formats, raw::get_clipboard_data, register_format, Clipboard, EnumFormats, Getter,
+};
+use image::{DynamicImage, ImageFormat};
 use std::fmt::Display;
 use std::io::Cursor;
-use std::{ffi::CStr, fs};
 use std::os::raw::c_char;
-use clipboard_win::raw::{set_without_clear};
-use clipboard_win::{SysResult, set_clipboard, get_clipboard_string};
-use clipboard_win::{Clipboard, formats, Getter, EnumFormats, register_format, raw::get_clipboard_data};
-use image::{ImageFormat, DynamicImage};
+use std::{ffi::CStr, fs};
 
 // use crate::windows;
 
@@ -18,114 +20,49 @@ pub enum CustomFormat {
     OemText,
 }
 
-pub fn read_image() -> image::DynamicImage {
-    let _clip = open_clipboard();
-    let mut out_image: Vec<u8> = Vec::new();
-
-    formats::Bitmap::read_clipboard(&formats::Bitmap, &mut out_image).expect("读取图片失败");
-    image::load_from_memory_with_format(&out_image, ImageFormat::Bmp).expect("图片转换失败")
-}
-
-pub fn write_image(img: DynamicImage) -> SysResult<()> {
-    let _clip = open_clipboard();
-    let cf_html: u32 = register_format("HTML Format").unwrap().into();
-    let mut buf: Vec<u8> = Vec::new();
-
-    img.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Bmp).expect("图片转换失败");
-    // formats::Bitmap::write_clipboard(&formats::Bitmap, &mut buf);
-    // formats::Unicode::write_clipboard(&formats::Unicode, &String::from("xdf"))
-    // let rd = formats::RawData(formats::CF_TEXT);
-    // rd.write_clipboard(&"xdfsfsadf").unwrap();
-
-    // let rd1 = formats::RawData(formats::CF_BITMAP);
-    // rd1.write_clipboard(&buf)
-
-    // set_clipboard(formats::Bitmap, &buf)?;
-    // close().unwrap();
-
-    // Clipboard::new_attempts(10).expect("打开剪贴板失败");
-    if let Err(e) = set_without_clear(formats::CF_TEXT, b"sdfsdf") {
-        println!("{:?}", e);
-    } else {
-        println!("No Error???")
-    }
-    if let Err(e) = set_without_clear(cf_html, wrap_html("<div>asdfasdfasdf</div>").as_bytes()) {
-        println!("{:?}", e);
-    } else {
-        println!("No Error???")
-    }
-
-    // if let Err(e) = set_without_clear(formats::CF_DIBV5, &buf) {
-    //     println!("{:?}", e);
-    // } else {
-    //     println!("No Error???")
-    // }
-    // let b = formats::Bitmap {};
-    // b.write_clipboard(&buf).unwrap();
-    
-    // set_bitmap_without_clear(&buf)?;
-    
-    // let mut ctx = arboard::Clipboard::new().unwrap();
-    // ctx.set_html(wrap_html("<div>ASDSSDF</div"), None).unwrap();
-    // ctx.set_text("text").unwrap();
-
-    Ok(())
-}
-
-pub fn read_html() -> String {
-    let _clip = open_clipboard();
-    let cf_html: u32 = register_format("HTML Format").unwrap().into();
-    read_custom_format_str(cf_html)
-}
-
-
 fn wrap_html(ctn: &str) -> String {
-	let h_version = "Version:0.9";
-	let h_start_html = "\r\nStartHTML:";
-	let h_end_html = "\r\nEndHTML:";
-	let h_start_frag = "\r\nStartFragment:";
-	let h_end_frag = "\r\nEndFragment:";
-	let c_start_frag = "\r\n<html>\r\n<body>\r\n<!--StartFragment-->\r\n";
-	let c_end_frag = "\r\n<!--EndFragment-->\r\n</body>\r\n</html>";
-	let h_len = h_version.len()
-		+ h_start_html.len()
-		+ 10 + h_end_html.len()
-		+ 10 + h_start_frag.len()
-		+ 10 + h_end_frag.len()
-		+ 10;
-	let n_start_html = h_len + 2;
-	let n_start_frag = h_len + c_start_frag.len();
-	let n_end_frag = n_start_frag + ctn.len();
-	let n_end_html = n_end_frag + c_end_frag.len();
-	format!(
-		"{}{}{:010}{}{:010}{}{:010}{}{:010}{}{}{}",
-		h_version,
-		h_start_html,
-		n_start_html,
-		h_end_html,
-		n_end_html,
-		h_start_frag,
-		n_start_frag,
-		h_end_frag,
-		n_end_frag,
-		c_start_frag,
-		ctn,
-		c_end_frag,
-	)
-}
-
-pub fn write_text(text: &str) -> SysResult<()> {
-    let _clip = open_clipboard();
-    set_clipboard(formats::Unicode, &text)
+    let h_version = "Version:0.9";
+    let h_start_html = "\r\nStartHTML:";
+    let h_end_html = "\r\nEndHTML:";
+    let h_start_frag = "\r\nStartFragment:";
+    let h_end_frag = "\r\nEndFragment:";
+    let c_start_frag = "\r\n<html>\r\n<body>\r\n<!--StartFragment-->\r\n";
+    let c_end_frag = "\r\n<!--EndFragment-->\r\n</body>\r\n</html>";
+    let h_len = h_version.len()
+        + h_start_html.len()
+        + 10
+        + h_end_html.len()
+        + 10
+        + h_start_frag.len()
+        + 10
+        + h_end_frag.len()
+        + 10;
+    let n_start_html = h_len + 2;
+    let n_start_frag = h_len + c_start_frag.len();
+    let n_end_frag = n_start_frag + ctn.len();
+    let n_end_html = n_end_frag + c_end_frag.len();
+    format!(
+        "{}{}{:010}{}{:010}{}{:010}{}{:010}{}{}{}",
+        h_version,
+        h_start_html,
+        n_start_html,
+        h_end_html,
+        n_end_html,
+        h_start_frag,
+        n_start_frag,
+        h_end_frag,
+        n_end_frag,
+        c_start_frag,
+        ctn,
+        c_end_frag,
+    )
 }
 
 fn read_custom_format_str(format: u32) -> String {
     let ptr = get_clipboard_data(format).expect(format!("读取{}失败！", format).as_str());
     let char_ptr: *const c_char = ptr.cast().as_ptr();
-    let cstr = unsafe {
-        CStr::from_ptr(char_ptr)
-    };
-    
+    let cstr = unsafe { CStr::from_ptr(char_ptr) };
+
     cstr.to_string_lossy().into_owned()
 }
 
@@ -141,7 +78,7 @@ impl Display for ClipboardData {
         let mut str = String::new();
 
         str.push_str("{");
-        
+
         if self.text != None {
             str.push_str("\ntext: ");
             str.push_str(self.text.clone().unwrap().as_str());
@@ -154,8 +91,10 @@ impl Display for ClipboardData {
         }
         if self.image != None {
             str.push_str("\nimage: ");
-            let img = self.image.clone().unwrap(); 
-            str.push_str(format!("Image (width: {}, height, {})", img.width(), img.height()).as_str());
+            let img = self.image.clone().unwrap();
+            str.push_str(
+                format!("Image (width: {}, height, {})", img.width(), img.height()).as_str(),
+            );
             str.push_str(",");
         }
         if self.files != None {
@@ -176,8 +115,8 @@ pub struct ClipboardFile {
     pub size: u64,
 }
 
-pub fn read_clipboard_data() -> ClipboardData {
-    let _clip = Clipboard::new_attempts(10).expect("打开剪贴板失败");
+pub fn read_clipboard_data() -> SysResult<ClipboardData> {
+    let _clip = open_clipboard();
     let ef = EnumFormats::new();
     let cf_html: u32 = register_format("HTML Format").unwrap().into();
     let mut result = ClipboardData {
@@ -189,7 +128,12 @@ pub fn read_clipboard_data() -> ClipboardData {
 
     ef.for_each(|x| {
         if x == formats::CF_BITMAP {
-            let img = read_image();
+            let mut out_image: Vec<u8> = Vec::new();
+
+            formats::Bitmap::read_clipboard(&formats::Bitmap, &mut out_image)
+                .expect("读取图片失败");
+            let img = image::load_from_memory_with_format(&out_image, ImageFormat::Bmp)
+                .expect("图片转换失败");
             result.image = Some(img);
             // img.save("./out.bmp").expect("写入图片失败");
         } else if x == formats::CF_TEXT {
@@ -200,16 +144,49 @@ pub fn read_clipboard_data() -> ClipboardData {
             result.html = Some(html);
         } else if x == formats::CF_HDROP {
             let mut file_contents = Vec::new();
-            let mut file_list: Vec<ClipboardFile> = Vec::new(); 
-            formats::FileList::read_clipboard(&formats::FileList, &mut file_contents).expect("读取文件失败");
+            let mut file_list: Vec<ClipboardFile> = Vec::new();
+            formats::FileList::read_clipboard(&formats::FileList, &mut file_contents)
+                .expect("读取文件失败");
             for item in file_contents {
                 let meta = fs::metadata(item.clone()).unwrap();
-                file_list.push(ClipboardFile { path: item, size: meta.len() })
+                file_list.push(ClipboardFile {
+                    path: item,
+                    size: meta.len(),
+                })
             }
             result.files = Some(file_list);
         }
-        
     });
 
-    result
+    Ok(result)
+}
+
+pub fn write_clipboard_data(data: ClipboardData, need_empty: bool) -> SysResult<()> {
+    let _clip = open_clipboard();
+
+    if need_empty {
+        empty()?;
+    }
+
+    if let Some(text) = data.text {
+        set_without_clear(formats::CF_TEXT, text.as_bytes())?;
+    }
+    if let Some(html) = data.html {
+        let cf_html: u32 = register_format("HTML Format").unwrap().into();
+        set_without_clear(cf_html, wrap_html(&html).as_bytes())?;
+    }
+    if let Some(image) = data.image {
+        let mut buf: Vec<u8> = Vec::new();
+        image
+            .write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Bmp)
+            .expect("图片转换失败");
+        formats::Bitmap.write_clipboard(&buf, false)?;
+    }
+    if let Some(files) = data.files {
+        // set_without_clear(formats::CF_HDROP, files.into_iter().map(|f| f.path))?;
+        let f: Vec<String> = files.into_iter().map(|f| f.path).collect();
+        formats::FileList.write_clipboard(&f, false).unwrap();
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
+use api::{ClipboardData, ClipboardFile};
+
 pub mod api;
 
 fn main() {
-    let img = image::open("D:/workspace/ws_rust/clipboardrs/out.bmp").unwrap();
-    api::write_image(img).unwrap();
+    let img =
+        image::open("C:/Users/vs429/Pictures/v2-d55549366b52cde19c93835cfa2a58c9_r.jpg").unwrap();
 
-    // api::write_text("xxx").unwrap();
+    let files = vec![ClipboardFile {
+        path: String::from("C:/Users/vs429/Downloads/21春本科工商管理(1)/21春本科工商管理/管理案例分析/形考任务3.docx"),
+        size: 0,
+    }];
 
-    // let res = api::read_clipboard_data();
-    // println!("{}", res);
+    let data = ClipboardData {
+        text: Some("qqqqq".to_string()),
+        html: Some("<div>xxxxx</div>".to_string()),
+        image: Some(img),
+        files: Some(files),
+    };
+
+    api::write_clipboard_data(data, true).unwrap();
 }
